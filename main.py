455a6b1fc45e4b3dcaed206e6d52a742f3348cc0
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-08-27 11:46:55 +0300

    tut 2

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+menu = ['One', 'Two', 'Three']
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!<h1>'
+    return render_template('index.html', title='Головна сторінка', menu=menu)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='Про сайт', menu=menu)
 
 
 if __name__ == '__main__':
