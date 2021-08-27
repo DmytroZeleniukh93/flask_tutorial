61ca807f45f666b4734e4e8060fec9d7564cfab1
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-08-27 18:03:50 +0300

    tut 5

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ['One', 'Two', 'Three']
+menu = [{'name': 'Головна сторінка', 'url': 'index'},
+        {'name': 'Про сайт', 'url': 'about'},
+        {'name': 'Контакти', 'url': 'contact'}]
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', title='Головна сторінка', menu=menu)
 
@@ -13,6 +16,13 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', title='Про сайт', menu=menu)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', menu=menu)
 
 
 if __name__ == '__main__':
